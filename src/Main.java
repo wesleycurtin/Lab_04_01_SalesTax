1cfb7e0a604bfb303b7457acc9983ddf6bdaa0b1
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-28 15:53:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int price = 102;
+        double tax = 0.05;
+        double total = (price * tax) + price;
+
+        System.out.println("The subtotal of your item(s) is " + price);
+        System.out.println("The sales tax is " + tax);
+        System.out.println("The total cost is " + total);
+
+
+
+
+
+
+
+
+    }
+}
